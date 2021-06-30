697f9cdb5b8d02d614262677aa4308039bf6bca7
margaiwangara <margaiwangara@gmail.com>
2021-06-30 10:27:14 +0300

    modified base url for connection to backend

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,7 +1,7 @@
 export const BASE_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:5000'
-    : 'http://mtandao-api.margaiwangara.me';
+    : 'https://mtandao-api.margaiwangara.me';
 export const GOOGLE_CLIENT_ID =
   '1013336741988-6nsb2n74ld4hov7q0uf90t21g518l99b.apps.googleusercontent.com';
 export const FACEBOOK_CLIENT_ID = '3107811909348613';
